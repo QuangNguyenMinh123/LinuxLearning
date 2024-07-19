234d2466f2aba5495b4ca38f55702b901d3328a3
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2024-07-19 17:42:39 +0700

    update

--- VI Socket/BT13/main_client.c
+++ VI Socket/BT13/main_client.c
@@ -7,8 +7,9 @@
 #include <pthread.h>
 /*******************************************************************/
 char bufSend[100] = {0};
-char bufReceive[100] = {0};
+char bufReceive[255] = {0};
 pthread_t threadSend, threadReceive;
+int sockD;
 /*******************************************************************/
 void *clientSend()
 {
@@ -17,17 +18,17 @@ void *clientSend()
 
 void *clientReceive()
 {
-
+    while (1)
+    {
+        recv(sockD, bufReceive, sizeof(bufReceive), 0);
+        printf("Message: %s", bufReceive);
+    }
 }
 /*******************************************************************/
 int main(int argc, char const* argv[]) 
 { 
-	int sockD = socket(AF_INET, SOCK_STREAM, 0);
+	sockD = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in servAddr;
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_port = htons(9001);        /* use some unused port number */ 
-	servAddr.sin_addr.s_addr = INADDR_ANY;
-	int connectStatus = connect(sockD, (struct sockaddr*)&servAddr, sizeof(servAddr));
     if (pthread_create(&threadSend,NULL,&clientSend,NULL))
     {
         printf("client sending thread is created\n");
@@ -36,16 +37,16 @@ int main(int argc, char const* argv[])
     {
         printf("client receiving thread is created\n");
     }
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(9001);        /* use some unused port number */ 
+	servAddr.sin_addr.s_addr = INADDR_ANY;
+	int connectStatus = connect(sockD, (struct sockaddr*)&servAddr, sizeof(servAddr));
 	if (connectStatus == -1) { 
 		printf("Error...\n");
 	}
-	else {
-        while (1)
-        {
-            char strData[255];
-            recv(sockD, strData, sizeof(strData), 0);
-            printf("Message: %s\n", strData);
-        }
-	}
+    int err = 0;
+    socklen_t size = sizeof (err);
+
+    while(1);
 	return 0;
 }
