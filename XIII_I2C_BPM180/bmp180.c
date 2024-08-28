c6ea012aa8a67316a3694cb98999856bb21bc05e
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2024-08-28 08:43:47 +0700

    update BMP180 without device tree

---
+++ XIII_I2C_BPM180/bmp180.c
@@ -0,0 +1,209 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/version.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/kernel.h>
+/*******************************************************************************/
+#define DRIVER_NAME 		"bmp180"
+#define DRIVER_CLASS 		"bmp180Class"
+/*******************************************************************************/
+static ssize_t bmp180_read(struct file *File, char *user_buffer, size_t count, loff_t *offs);
+/*******************************************************************************/
+/* Defines for device identification */ 
+#define I2C_BUS				2			/* The I2C Bus available on the raspberry */
+#define BMP180_NAME			"BMP180"	/* Device and Driver Name */
+#define BMP180_ADDRESS		0x77		/* BMP180 I2C address */
+
+static const struct i2c_device_id bmp_id[] = {
+		{ BMP180_NAME, 0 }, 
+		{ }
+};
+
+static struct i2c_driver bmp_driver = {
+	.driver = {
+		.name = BMP180_NAME,
+		.owner = THIS_MODULE
+	}
+};
+
+static struct i2c_board_info bmp180_i2c_board_info = {
+	I2C_BOARD_INFO(BMP180_NAME, BMP180_ADDRESS)
+};
+
+/* Map the file operations */
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = bmp180_read,
+};
+
+/* Variables for Device and Deviceclass*/
+static dev_t myDeviceNr;
+static struct class *myClass;
+static struct cdev myDevice;
+
+/* Variables for temperature calculation */
+s32 dig_T1, dig_T2, dig_T3;
+
+static struct i2c_adapter * bmp180_i2c_adapter = NULL;
+static struct i2c_client  * bmp180_i2c_client = NULL;
+/*******************************************************************************/
+/**
+ * @brief Read current temperature from BMP180 sensor
+ *
+ * @return temperature in degree
+ */
+s32 read_temperature(void) {
+	int var1, var2;
+	s32 raw_temp;
+	s32 d1, d2, d3;
+
+	/* Read Temperature */
+	d1 = i2c_smbus_read_byte_data(bmp180_i2c_client, 0xF6);
+	d2 = i2c_smbus_read_byte_data(bmp180_i2c_client, 0xF7);
+	d3 = i2c_smbus_read_byte_data(bmp180_i2c_client, 0xF8);
+	raw_temp = ((d1<<16) | (d2<<8) | d3) >> 4;
+
+	/* Calculate temperature in degree */
+	var1 = ((((raw_temp >> 3) - (dig_T1 << 1))) * (dig_T2)) >> 11;
+
+	var2 = (((((raw_temp >> 4) - (dig_T1)) * ((raw_temp >> 4) - (dig_T1))) >> 12) * (dig_T3)) >> 14;
+	return ((var1 + var2) *5 +128) >> 8;
+}
+
+/**
+ * @brief Get data out of buffer
+ */
+static ssize_t bmp180_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
+	int to_copy, not_copied, delta;
+	char out_string[20];
+	int temperature;
+	printk("Reading temp from bmp180\n");
+	/* Get amount of bytes to copy */
+	to_copy = min(sizeof(out_string), count);
+
+	/* Get temperature */
+	temperature = read_temperature();
+	snprintf(out_string, sizeof(out_string), "%d.%d\n", temperature/100, temperature%100);
+
+	/* Copy Data to user */
+	not_copied = copy_to_user(user_buffer, out_string, to_copy);
+
+	/* Calculate delta */
+	delta = to_copy - not_copied;
+
+	return delta;
+}
+
+/**
+ * @brief function, which is called after loading module to kernel, do initialization there
+ */
+static int __init bmp180_Init(void) {
+	int ret = -1;
+	u8 id;
+	printk("MyDeviceDriver - Hello Kernel\n");
+
+	/* Allocate Device Nr */
+	if ( alloc_chrdev_region(&myDeviceNr, 0, 1, DRIVER_NAME) < 0) {
+		printk("Device Nr. could not be allocated!\n");
+	}
+	printk("MyDeviceDriver - Device Nr %d was registered\n", myDeviceNr);
+
+	/* Create Device Class */
+	if ((myClass = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+		printk("Device Class can not be created!\n");
+		goto ClassError;
+	}
+
+	/* Create Device file */
+	if (device_create(myClass, NULL, myDeviceNr, NULL, DRIVER_NAME) == NULL) {
+		printk("Can not create device file!\n");
+		goto FileError;
+	}
+
+	/* Initialize Device file */
+	cdev_init(&myDevice, &fops);
+
+	/* register device to kernel */
+	if (cdev_add(&myDevice, myDeviceNr, 1) == -1) {
+		printk("Registering of device to kernel failed!\n");
+		goto KernelError;
+	}
+
+	bmp180_i2c_adapter = i2c_get_adapter(I2C_BUS);
+
+	if(bmp180_i2c_adapter != NULL) {
+		bmp180_i2c_client = i2c_new_client_device(bmp180_i2c_adapter, &bmp180_i2c_board_info);
+		if(bmp180_i2c_client != NULL) {
+			if(i2c_add_driver(&bmp_driver) != -1) {
+				ret = 0;
+				printk("OK.\n");
+			}
+			else
+				printk("Can't add driver...\n");
+		}
+		else
+			printk("bmp180_i2c_client = NULL\n");
+	}
+	else
+		printk("bmp180_i2c_adapter = NULL\n");
+	printk("BMP180 Driver added!\n");
+
+	/* Read Chip ID */
+	id = i2c_smbus_read_byte_data(bmp180_i2c_client, 0xD0);
+	printk("11111!\n");
+	printk("ID: 0x%x\n", id);
+	/* Read Calibration Values */
+	dig_T1 = i2c_smbus_read_word_data(bmp180_i2c_client, 0x88);
+	printk("dig_T1: %d\n", dig_T1);
+	dig_T2 = i2c_smbus_read_word_data(bmp180_i2c_client, 0x8a);
+	printk("dig_T2: %d\n", dig_T2);
+	dig_T3 = i2c_smbus_read_word_data(bmp180_i2c_client, 0x8c);
+	printk("dig_T3: %d\n", dig_T3);
+
+	if(dig_T2 > 32767)
+		dig_T2 -= 65536;
+
+	if(dig_T3 > 32767)
+		dig_T3 -= 65536;
+
+	/* Initialice the sensor */
+	i2c_smbus_write_byte_data(bmp180_i2c_client, 0xf5, 5<<5);
+	i2c_smbus_write_byte_data(bmp180_i2c_client, 0xf4, ((5<<5) | (5<<2) | (3<<0)));
+	printk("Done Init\n");
+	return ret;
+
+KernelError:
+	device_destroy(myClass, myDeviceNr);
+FileError:
+	class_destroy(myClass);
+ClassError:
+	unregister_chrdev(myDeviceNr, DRIVER_NAME);
+	return (-1);
+}
+
+/**
+ * @brief function, which is called when removing module from kernel
+ * free alocated resources
+ */
+static void __exit bmp180_Exit(void) {
+	printk("MyDeviceDriver - Goodbye, Kernel!\n");
+	i2c_unregister_device(bmp180_i2c_client);
+	i2c_put_adapter(bmp180_i2c_adapter);
+	i2c_del_driver(&bmp_driver);
+	cdev_del(&myDevice);
+    device_destroy(myClass, myDeviceNr);
+    class_destroy(myClass);
+    unregister_chrdev_region(myDeviceNr, 1);
+}
+/*******************************************************************************/
+module_init(bmp180_Init);
+module_exit(bmp180_Exit);
+/* Meta Information */
+MODULE_AUTHOR("Johannes 4Linux");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("A driver for reading out a BMP180 temperature sensor");
+/*******************************************************************************/
