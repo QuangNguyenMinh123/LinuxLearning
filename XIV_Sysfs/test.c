0c1dcd5f157ec6406295264201dd2f13a4205e70
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2024-09-11 13:35:01 +0700

    update

--- XIV_Sysfs/test.c
+++ XIV_Sysfs/test.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
-#include <system.h>
+#include <time.h>
+#include <errno.h> 
+#include <stdlib.h>
 /*******************************************************************************/
 #define VALUE_PATH          "/sys/bbb_gpio/value"
 #define DIRECTION_PATH      "/sys/bbb_gpio/direction"
 /*******************************************************************************/
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+
+    do {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
 
 /*******************************************************************************/
 int main(void)
 {
+    while (1)
+    {
+        system("cat /sys/bbb_gpio/value");
+        msleep(100);
+    }
     
     return 0;
 }
