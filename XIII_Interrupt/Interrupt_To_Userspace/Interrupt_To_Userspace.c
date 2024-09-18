73882e34303c8a046417e465e6ae08111da12e23
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2024-09-18 08:07:24 +0700

    update Interrupt to user space

--- XIII_Interrupt/Interrupt_To_Userspace/Interrupt_To_Userspace.c
+++ XIII_Interrupt/Interrupt_To_Userspace/Interrupt_To_Userspace.c
@@ -10,7 +10,8 @@
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <linux/pinctrl/consumer.h>
-
+#include <linux/sched/signal.h>
+#include <linux/ioctl.h>
 /*******************************************************************************/
 /* Meta Information */
 MODULE_LICENSE("GPL");
@@ -19,6 +20,12 @@ MODULE_DESCRIPTION("A simple LKM for a gpio interrupt");
 /*******************************************************************************/
 #define noLed				5
 #define COMPATIBLE			"Interrupt_Pinmux"
+#define REGISTER_UAPP 		_IO('R', 'g')
+#define SIGNR0 				44
+#define SIGNR1 				45
+#define SIGNR2 				46
+#define SIGNR3 				47
+#define SIGNR4 				48
 /*******************************************************************************/
 /* Function prototype */
 static int dt_probe(struct platform_device *pdev);
@@ -34,6 +41,7 @@ static irqreturn_t (*funcPtr[noLed]) (int irq, void *dev_id) = {
 	gpio_irq_handler3,
 	gpio_irq_handler4
 };
+static long int my_ioctl(struct file *file, unsigned cmd, unsigned long arg);
 /*******************************************************************************/
 /* Data struct for device tree*/
 static struct gpio_desc *descGPIO[noLed] = {NULL};
@@ -53,34 +61,90 @@ static struct platform_driver my_driver = {
 		.of_match_table = of_match_ptr(my_driver_id),
 	}
 };
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.release = NULL,
+	.unlocked_ioctl = my_ioctl,
+};
 unsigned int irq_number[noLed] = {0};
+static struct task_struct *task = NULL;
+static struct proc_dir_entry *proc_file;
 /*******************************************************************************/
 /**
  * @brief Interrupt service routine is called, when interrupt is triggered
  */
 static irqreturn_t gpio_irq_handler0(int irq, void *dev_id)
 {
-	printk("gpio_irq: Interrupt 0!\n");
+	struct siginfo info;
+	if(task != NULL) {
+		memset(&info, 0, sizeof(info));
+		info.si_signo = SIGNR0;
+		info.si_code = SI_QUEUE;
+
+		/* Send the signal */
+		if(send_sig_info(SIGNR0, (struct kernel_siginfo *) &info, task) < 0)
+			printk("gpio_irq_signal: Error sending signal\n");
+	}
 	return IRQ_HANDLED;
 }
+
 static irqreturn_t gpio_irq_handler1(int irq, void *dev_id)
 {
-	printk("gpio_irq: Interrupt 1!\n");
+	struct siginfo info;
+	if(task != NULL) {
+		memset(&info, 0, sizeof(info));
+		info.si_signo = SIGNR1;
+		info.si_code = SI_QUEUE;
+
+		/* Send the signal */
+		if(send_sig_info(SIGNR1, (struct kernel_siginfo *) &info, task) < 0)
+			printk("gpio_irq_signal: Error sending signal\n");
+	}
 	return IRQ_HANDLED;
 }
+
 static irqreturn_t gpio_irq_handler2(int irq, void *dev_id)
 {
-	printk("gpio_irq: Interrupt 2!\n");
+	struct siginfo info;
+	if(task != NULL) {
+		memset(&info, 0, sizeof(info));
+		info.si_signo = SIGNR2;
+		info.si_code = SI_QUEUE;
+
+		/* Send the signal */
+		if(send_sig_info(SIGNR2, (struct kernel_siginfo *) &info, task) < 0)
+			printk("gpio_irq_signal: Error sending signal\n");
+	}
 	return IRQ_HANDLED;
 }
+
 static irqreturn_t gpio_irq_handler3(int irq, void *dev_id)
 {
-	printk("gpio_irq: Interrupt 3!\n");
+	struct siginfo info;
+	if(task != NULL) {
+		memset(&info, 0, sizeof(info));
+		info.si_signo = SIGNR3;
+		info.si_code = SI_QUEUE;
+
+		/* Send the signal */
+		if(send_sig_info(SIGNR3, (struct kernel_siginfo *) &info, task) < 0)
+			printk("gpio_irq_signal: Error sending signal\n");
+	}
 	return IRQ_HANDLED;
 }
+
 static irqreturn_t gpio_irq_handler4(int irq, void *dev_id)
 {
-	printk("gpio_irq: Interrupt 4!\n");
+	struct siginfo info;
+	if(task != NULL) {
+		memset(&info, 0, sizeof(info));
+		info.si_signo = SIGNR4;
+		info.si_code = SI_QUEUE;
+
+		/* Send the signal */
+		if(send_sig_info(SIGNR4, (struct kernel_siginfo *) &info, task) < 0)
+			printk("gpio_irq_signal: Error sending signal\n");
+	}
 	return IRQ_HANDLED;
 }
 /*******************************************************************************/
@@ -119,8 +183,29 @@ static int dt_probe(struct platform_device *pdev)
 			return -1;
 		}
 	}
-
+	/* Register file operations to this driver */
+	proc_file = proc_create_data("InterruptFromKernel", 0666, NULL, &fops, NULL);
+	
+	if(proc_file == NULL) {
+		printk("dt_probe - Error creating /proc/InterruptFromKernel\n");
+		for (i = 0;i < noLed; i++)
+		{
+			gpiod_put(descGPIO[i]);
+		}
+		return -ENOMEM;
+	}
 	printk("Done!\n");
+	return 0;
+}
+/**
+ * @brief This function is called to save process ID to task
+ */
+static long int my_ioctl(struct file *file, unsigned cmd, unsigned long arg)
+{
+	if(cmd == REGISTER_UAPP) {
+		task = get_current();
+		printk("gpio_irq_signal: Userspace app with PID %d is registered\n", task->pid);
+	}
 	return 0;
 }
 /**
@@ -151,6 +236,7 @@ static void __exit ModuleExit(void)
 		gpiod_put(descGPIO[i]);
 	}
 	platform_driver_unregister(&my_driver);
+	proc_remove(proc_file);
 }
 /*******************************************************************************/
 module_init(ModuleInit);
--- XIII_Interrupt/Interrupt_To_Userspace/app_interrupt.c
+++ XIII_Interrupt/Interrupt_To_Userspace/app_interrupt.c
@@ -5,11 +5,32 @@
 #include <sys/ioctl.h>
 #include <signal.h>
 /*******************************************************************************/
-#define SIGTX 44
-#define REGISTER_UAPP _IO('R', 'g')
+#define SIGTX0               44
+#define SIGTX1               45
+#define SIGTX2               46
+#define SIGTX3               47
+#define SIGTX4               48
+#define REGISTER_UAPP 		_IO('R', 'g')
+#define FILE_PATH           "/proc/InterruptFromKernel"
 /*******************************************************************************/
-void signalhandler(int sig) {
-	printf("Button was pressed!\n");
+void signalhandler0(int sig) {
+	printf("Interrupt 0 from kernel!\n");
+}
+
+void signalhandler1(int sig) {
+	printf("Interrupt 1 from kernel!\n");
+}
+
+void signalhandler2(int sig) {
+	printf("Interrupt 2 from kernel!\n");
+}
+
+void signalhandler3(int sig) {
+	printf("Interrupt 3 from kernel!\n");
+}
+
+void signalhandler4(int sig) {
+	printf("Interrupt 4 from kernel!\n");
 }
 /*******************************************************************************/
 
@@ -17,8 +38,30 @@ void signalhandler(int sig) {
 int main (void)
 {
     int fd;
-	signal(SIGTX, signalhandler);
+	signal(SIGTX0, signalhandler0);
+    signal(SIGTX1, signalhandler1);
+    signal(SIGTX2, signalhandler2);
+    signal(SIGTX3, signalhandler3);
+    signal(SIGTX4, signalhandler4);
     printf("This is user-space application. PID: %d\n", getpid());
-    fd = open("/dev/irq_signal", O_RDONLY);
+    fd = open(FILE_PATH, O_RDONLY);
+    if(fd < 0) {
+		perror("Could not open /dev/InterruptFromKernel");
+		return -1;
+	}
+    /* Send PID to kernel via ioctl */
+    if(ioctl(fd, REGISTER_UAPP, NULL)) {
+		perror("Error registering app");
+		close(fd);
+		return -1;
+	}
+    printf("Process ID is sent to kernel\n");
+    /* Wait for Signal */
+	printf("Waiting for signal...\n");
+    while (1)
+    {
+
+    }
+    close(fd);
     return 0;
 }
