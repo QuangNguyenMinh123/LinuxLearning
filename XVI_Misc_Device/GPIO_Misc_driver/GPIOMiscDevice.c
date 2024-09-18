897d53731910761491bc006a1d79a96403cfc43f
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2024-09-18 10:00:47 +0700

    update

--- XVI_Misc_Device/GPIO_Misc_driver/GPIOMiscDevice.c
+++ XVI_Misc_Device/GPIO_Misc_driver/GPIOMiscDevice.c
@@ -3,32 +3,23 @@
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
 #include <linux/uaccess.h>
+#include <linux/pinctrl/consumer.h>
+#include <linux/gpio.h>
+#include <linux/platform_device.h>
+#include <linux/of_device.h>
 /*******************************************************************************/
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Johannes 4 GNU/Linux");
 MODULE_DESCRIPTION("A simple demonstration for a misc device");
 /*******************************************************************************/
-#define MAX_SIZE 256
-#define COMPATIBLE				"BMP180MiscDevice"
+#define MAX_SIZE 				256
+#define COMPATIBLE				"GPIOMiscDevice"
 /*******************************************************************************/
 static char data[MAX_SIZE];
 static size_t data_len;
 /*******************************************************************************/
-static int my_open(struct inode *inode, struct file *file) {
-	printk("misc_test - Open called\n");
-	printk("misc_test - Device Numbers: %d %d\n", imajor(inode), iminor(inode));
-	if(file->f_mode & FMODE_READ) 
-		printk("misc_test - Open called with read permissions\n");
-	if(file->f_mode & FMODE_WRITE) 
-		printk("misc_test - Open called with write permissions\n");
-	return 0;
-}
-
-static int my_close(struct inode *inode, struct file *file) {
-	printk("misc_test - Close called\n");
-	return 0;
-}
-
+static struct gpio_desc *descGPIO = NULL;
+/*******************************************************************************/
 static ssize_t my_write(struct file *file, const char __user *user_buffer, size_t user_len, loff_t *ppos) {
 	int status;
 	if(user_len > data_len)
@@ -41,31 +32,23 @@ static ssize_t my_write(struct file *file, const char __user *user_buffer, size_
 		printk("misc_test - Error during copy_from_user\n");
 		return -status;
 	}
-	return data_len;
-}
+	if (data[0] == "1")
+	{
 
-static ssize_t my_read(struct file *file, char __user *user_buffer, size_t user_len, loff_t *ppos) {
-	int status;
-	size_t len;
-	if(user_len < data_len)
-		len = user_len;
-	else
-		len = data_len;
-	printk("misc_test - Read called\n");
-	status = copy_to_user(user_buffer, data, len);
-	if(status) {
-		printk("misc_test - Error during copy_to_user\n");
-		return -status;
 	}
-	return len;
+	else if (data[0] == "0")
+	{
+
+	}
+	return data_len;
 }
 /*******************************************************************************/
 static const struct file_operations fops = {
 	.owner = THIS_MODULE,
-	.read = my_read,
+	.read = NULL,
 	.write = my_write,
-	.open = my_open,
-	.release = my_close,
+	.open = NULL,
+	.release = NULL,
 };
 
 static struct miscdevice my_device = {
@@ -73,7 +56,17 @@ static struct miscdevice my_device = {
 	.minor = MISC_DYNAMIC_MINOR,
 	.fops = &fops,
 };
+
+static struct platform_driver my_driver = {
+	.probe = dt_probe,
+	.remove = NULL,
+	.driver = {
+		.name = "MiscDriver_for_GPIO",
+		.of_match_table = of_match_ptr(my_driver_id),
+	}
+};
 /*******************************************************************************/
+
 /**
  * @brief This function is called, when the module is loaded into the kernel
  */
