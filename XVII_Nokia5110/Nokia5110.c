22eb48d6a149d93f9d63b2e269fadfc0e968c279
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2024-09-24 22:56:29 +0700

    update

--- XVII_Nokia5110/Nokia5110.c
+++ XVII_Nokia5110/Nokia5110.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/gpio.h>
 #include <linux/spi/spi.h>
+#include <linux/delay.h>
 /*******************************************************************************/
 /* Meta Information */
 MODULE_LICENSE("GPL");
@@ -12,17 +13,149 @@ MODULE_DESCRIPTION("A SPI driver for Nokia5110 LCD");
 #define HIGH				1
 #define COMPATIBLE			"QuangNM13,Nokia5110"
 /*******************************************************************************/
+#define POWER_ON					0
+#define POWER_OFF					1 << 2
+#define HORIZONTAL_ADDRESS			0
+#define VERTICAL_ADDRESS			1 << 1
+#define BASIC_INSTRUCTION			0
+#define EXTEND_INSTRUCTION			1 << 0
+#define DISPLAY_BLANK				0
+#define NORMAL_MODE					0b100
+#define ALL_SEG_ON					0b001
+#define INVERSED_MODE				0b101
+/*******************************************************************************/
+typedef enum DisplayModeType{
+	BLANK 	= 0b00001000,
+	NORMAL 	= 0b00001100,
+	ALL_SEG = 0b00001001,
+	INVERSE	= 0b00001101,
+}DisplayModeType;
+/*******************************************************************************/
+char image[504] = {
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111, 
+	0b00011111, 0b00000101, 0b00000111, 
+	0b00000000, 
+	0b00011111, 0b00000100, 0b00011111
+};
+/*******************************************************************************/
+
+/*******************************************************************************/
 static struct gpio_desc *resetPin = NULL;
 static struct gpio_desc *dcPin = NULL;
+static struct spi_device *nokia5110 = NULL;
 /*******************************************************************************/
 static int Nokia5110_probe(struct spi_device *pdev);
 static int Nokia5110_remove(struct spi_device *pdev);
 /*******************************************************************************/
+static void Nokia5110_Write(struct spi_device *pdev, bool isCommand, char* buff, int size)
+{
+	if (isCommand)						/* If command is sent */
+		gpiod_set_value(dcPin, LOW);
+	else								/* If data is sent */
+		gpiod_set_value(dcPin, HIGH);
+	spi_write(pdev, buff, size);
+	gpiod_set_value(dcPin, LOW);
+}
+
+static void Nokia5110_Reset(void)
+{
+	gpiod_set_value(resetPin, LOW);
+	mdelay(150);
+	gpiod_set_value(resetPin, HIGH);
+}
+
+static void Nokia5110_goto(struct spi_device *pdev, int x, int y)
+{
+	char buff[3] = {
+		0b00100000 | POWER_OFF | HORIZONTAL_ADDRESS | BASIC_INSTRUCTION,
+		0b10000000 | x,		/* Set x address, 0 <= x <= 83*/
+		0b01000000 | y		/* Set x address, 0 <= y <= 5*/
+	};
+	Nokia5110_Write(pdev, true, buff, sizeof(buff));
+}
+
+static void Nokia5110_print(struct spi_device *pdev, char* ch, int size)
+{
+	Nokia5110_Write(pdev, false, ch, size);
+}
+
+static void Nokia5110_ClearScreen(struct spi_device *pdev)
+{
+	char buff[504] = {0};
+	Nokia5110_goto(pdev, 0, 0);
+	Nokia5110_print(pdev, buff, 504);
+}
+
+static void Nokia5110_Init(struct spi_device *pdev)
+{
+	char buff[] = {
+		0x21,		/* Function set */
+		0b10000000 | 100,
+		0x20,			/* Basic command */
+		0x0C
+	};
+	Nokia5110_Reset();
+	Nokia5110_Write(pdev, true, buff, sizeof(buff));
+}
+
+static void Nokia5110_Deinit(struct spi_device *pdev)
+{
+	char buff[] = {
+		0b00100000 | POWER_OFF | HORIZONTAL_ADDRESS | EXTEND_INSTRUCTION,
+		
+	};
+	
+}
+
+
 
 /*******************************************************************************/
 
 /*******************************************************************************/
-
 static struct of_device_id nokia5110_id[] = {
 	{
 		.compatible = COMPATIBLE,
@@ -54,19 +187,28 @@ static int Nokia5110_probe(struct spi_device *pdev)
 		printk("dt_probe - Error! Device property 'reset-gpio' not found\n");
 	}
 	/* Obtain GPIO */
-	resetPin = gpiod_get(dev, "reset", GPIOD_OUT_LOW);
+	resetPin = gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(resetPin))
 	{
 		printk("Nokia5110 Probe - Error! cannot setup the reset-gpio\n");
 		gpiod_put(resetPin);
 	}
-	dcPin = gpiod_get(dev, "commandData", GPIOD_OUT_LOW);
+	gpiod_set_value(resetPin, HIGH);
+	dcPin = gpiod_get(dev, "commandData", GPIOD_OUT_HIGH);
 	if (IS_ERR(dcPin))
 	{
 		printk("Nokia5110 Probe - Error! cannot setup the dcPin\n");
 		gpiod_put(dcPin);
 	}
+	gpiod_set_value(dcPin, HIGH);
 	/* Obtain SPI device */
+	nokia5110 = pdev;
+	Nokia5110_Init(nokia5110);
+	mdelay(1000);
+	Nokia5110_ClearScreen(nokia5110);
+	
+	Nokia5110_goto(nokia5110, 0, 0);
+	Nokia5110_print(nokia5110, image, 200);
 	return 0;
 }
 /**
@@ -83,4 +225,4 @@ static int Nokia5110_remove(struct spi_device *pdev)
 
 /*******************************************************************************/
 
-
+/*******************************************************************************/
